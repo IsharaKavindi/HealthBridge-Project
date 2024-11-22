2107c070cb3b1e112eaaabcb2a9f8525e86c5c24
IsharaKavindi <isharajayasundara668@gmail.com>
2024-11-22 19:05:36 +0530

    Backend

--- staffDashbord/staffProfile.php
+++ staffDashbord/staffProfile.php
@@ -81,10 +81,10 @@ mysqli_close($conn);
                         <div class="patientRegister_div">
                             <label for="profileImage" class="upload-label">
                             <?php
-                                if ($image != "") {
+                                if ($staffImage != "") {
                                     // Display the image
                                     ?>
-                                    <img class="img" src="img/<?php echo $staffImage; ?>" alt="<?php echo $$staffImage; ?>">
+                                    <img class="img" src="./img/<?php echo $staffImage; ?>" alt="<?php echo $staffImage; ?>">
                                     <?php
                                 } else {
                                     echo "<div class='error'>Image not available.</div>";
@@ -93,12 +93,17 @@ mysqli_close($conn);
                             </label>
                             <input class="search_icn" type="file" id="staffImage" name="staffImage" accept="image/*"><br><br>
                             <label>Title</label>
+                            <?php
+                            // Ensure $staffTitle is initialized
+                            $staffTitle = isset($staffTitle) ? $staffTitle : ''; // Replace with database or form value if applicable
+                            ?>
                             <select class="search_icn" id="staffTitle" name="staffTitle">
                                 <option <?php if ($staffTitle == 'Mr') echo 'selected'; ?>>Mr</option>
                                 <option <?php if ($staffTitle == 'Ms') echo 'selected'; ?>>Ms</option>
                                 <option <?php if ($staffTitle == 'Mrs') echo 'selected'; ?>>Mrs</option>
                             </select>
-                        </div>
+
+                            </div>
                         <div class="patientRegister_div">
                             <label>First Name</label>
                             <input class="search_icn" type="text" name="staffFirstName" value="<?php echo htmlspecialchars($staffFirstName); ?>"><br>
