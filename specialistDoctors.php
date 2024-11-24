9c3211bc0fe90556f386aa3dfb3b8cff44c6355d
Roshelvimukthi <roshelfernando.v@gmail.com>
2024-11-24 12:51:46 +0530

    Backend specialization doctors

--- specialistDoctors.php
+++ specialistDoctors.php
@@ -25,16 +25,21 @@ $result = mysqli_query($conn, $query);
 // Build the HTML response
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
+        // Check and sanitize the image path
+        $imagePath = !empty($row['doctorImage']) ? htmlspecialchars($row['doctorImage']) : "img/default.jpg";
+    
         echo '<div class="Specialist">';
-        echo '<img class="img1" src="' . $row['doctorImage'] . '" alt="Dr. ' . htmlspecialchars($row['doctorFirstname']) . '">';
+        echo '<a href="doctorDetails.php?doctorID=' . urlencode($row['doctorID']) . '">';
+        echo '<img class="img1" src="' . $imagePath . '" alt="Dr. ' . htmlspecialchars($row['doctorFirstname']) . '" style="width: 100px; height: 100px; border-radius: 50%;">';
         echo '<h3 class="dr_name">Dr. ' . htmlspecialchars($row['doctorFirstname']) . ' ' . htmlspecialchars($row['doctorLastname']) . '</h3>';
+        echo '</a>';
         echo '<p class="dr_special">' . htmlspecialchars($row['doctorSpecialization']) . '</p>';
         echo '</div>';
     }
+    
 } else {
     echo '<p>No specialists found for this specialization.</p>';
 }
 
 mysqli_close($conn);
 ?>
-
